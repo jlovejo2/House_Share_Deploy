31ef6f89228575c5c40f27fa0ef1096a6df5e9f4
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-19 17:04:59 -0500

    removed dotenv code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
+
 // const express = require("express");
 //typeScript supports import
 const express_1 = __importDefault(require("express"));
@@ -25,7 +51,8 @@ const graphql_1 = require("./graphql");
 //makes database connection more apparent
 //the app has type that is the Application imported from express
 //mount function excepts the express app instance
-const mount = (app) => __awaiter(void 0, void 0, void 0, function* () {
+const mount = (app) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const db = yield database_1.connectDatabase();
     app.use(body_parser_1.default.json({ limit: "2mb" }));
     app.use(cookie_parser_1.default(process.env.SECRET));
@@ -33,15 +60,17 @@ const mount = (app) => __awaiter(void 0, void 0, void 0, function* () {
     //express method for serving static files
     app.use(express_1.default.static(`${__dirname}/client`));
     //tells every route user enters to serve same index.html file
-    app.get("/*", (_req, res) => res.sendFile(`${__dirname}/client/index.html`));
+    app.get("/*", (_req, res) =>
+      res.sendFile(`${__dirname}/client/index.html`)
+    );
     //creating instance of ApolloServer
     //passing in options needed to instantiate the apolloServer instance
     //ApolloSever expects an existing schema or object with typeDefs and resolvers to be passed into it
     //context option allows all resolvers to have access to connecting to the db
     const server = new apollo_server_express_1.ApolloServer({
-        typeDefs: graphql_1.typeDefs,
-        resolvers: graphql_1.resolvers,
-        context: ({ req, res }) => ({ db, req, res }),
+      typeDefs: graphql_1.typeDefs,
+      resolvers: graphql_1.resolvers,
+      context: ({ req, res }) => ({ db, req, res }),
     });
     //specify middleware to work alongisde existing server middleware
     //passing in express app instance and where graphql api should live
@@ -51,7 +80,7 @@ const mount = (app) => __awaiter(void 0, void 0, void 0, function* () {
     //The below two lines of code are used to check that the connection to the database is made and the data is then console logged
     // const listings = await db.listings.find({}).toArray();
     // console.log(listings);
-});
+  });
 //dismount function runs the express()
 mount(express_1.default());
 //______________________________________________
